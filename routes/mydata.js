26c7ba4618cd4322205f30d85d578744c7dbd0f7
Chandra Adithya Pamidala <111883869+Adi758@users.noreply.github.com>
2022-10-24 13:49:56 -0500

    point

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Chandra Adithya Pamidala' });
+  res.render('mydata', { title: 'Chandra Adithya Pamidala' });
 });
 
 module.exports = router;
